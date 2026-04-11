feb0f7c8659127b52120eb27cae38977d3ea014c
pHzin <pedroroccha2008@gmail.com>
2026-04-11 01:37:26 -0300

    DarkMode

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const botao = document.getElementsByTagName('button')[0]
 const menu = document.getElementsByTagName('nav')[0]
+const botaoLight = document.getElementById('light')
 
 function abreMenu() {
-    
     menu.style.zIndex = '0'
     menu.style.opacity = '100'
 
@@ -19,3 +19,14 @@ function fechaMenu() {
 }
 
 botao.addEventListener('click', abreMenu)
+botaoLight.addEventListener('click', () => {
+    if (botaoLight.checked == true) {
+        document.documentElement.style.setProperty('--cor-fundo', '#0e1014')
+        document.documentElement.style.setProperty('--cor-fundoMenu', '#808080f1')
+        document.documentElement.style.setProperty('--cor-fonte', '#fff')
+    } else {
+        document.documentElement.style.setProperty('--cor-fundo', '#fff')
+        document.documentElement.style.setProperty('--cor-fundo', '#d3d3d3ea')
+        document.documentElement.style.setProperty('--cor-fonte', '#0e1014')
+    }
+})
